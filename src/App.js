8f014f9cbf9ad31f7cfd82bcc2ab26b0ad5b7c22
Alexandra Morozoff <a.j.morozoff@gmail.com>
2020-05-09 11:32:18 -0400

    exploring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import nlp from 'compromise';
 import './App.css';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [names, setNames] = useState('');
+  const [verbs, setVerbs] = useState('');
+  const [orgs, setOrgs] = useState('');
+
+  const handleChange = (value) => {
+    const doc = nlp(value);
+    const foundVerbs = doc.verbs().out('array');
+    const foundOrgs = doc.organizations().out('array');
+    const foundNames = doc.people().out('array');
+
+    foundVerbs.length && setVerbs(foundVerbs);
+    foundOrgs.length && setOrgs(foundOrgs);
+    foundNames.length && setNames(foundNames);
+    
+    setInputText(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor='compromise-input'>A text input</label>
+        <textarea
+          id='compromise-input'
+          rows='10'
+          onChange={(e) => handleChange(e.target.value)}
+          value={inputText}
+        />
+      </div>
+      <div className="termsBlock">
+          {
+            names.length ?
+            names.map(name => 
+              <div className=".tagInput">
+                Tag {name}
+                <input
+                  type="text"
+                  value={'@'} 
+                />
+              </div>
+            )
+            : null
+          }
+      </div>
+      <div className="termsBlock">
+          {
+            orgs.length ?
+            orgs.map(org => <span> {org} </span>)
+            : null
+          }
+      </div>
     </div>
   );
 }
